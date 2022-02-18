2aba644d2c839b0a10124bfde4fbaf658d89c776
Iridero <jcgarza_mx@hotmail.com>
2022-02-18 09:50:08 -0600

    Agregar archivos de proyecto.

---
+++ ED.TorresDeHanoi/FrmHanoi.cs
@@ -0,0 +1,15 @@
+namespace ED.TorresDeHanoi
+{
+    public partial class FrmHanoi : Form
+    {
+        public FrmHanoi()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmHanoi_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ED.TorresDeHanoi/Juego.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ED.TorresDeHanoi
+{
+    
+
+    internal class Juego
+    {
+        private Stack<byte> PosteA;
+        private Stack<byte> PosteB;
+        private Stack<byte> PosteC;
+
+        private bool EstaIniciado = false;
+
+        /// <summary>
+        /// Inicializa el juego
+        /// </summary>
+        /// <param name="NumDiscos">El número de discos que se usarán (Máx 9)</param>
+        public void Iniciar(byte NumDiscos)
+        {
+            if (NumDiscos==0 || NumDiscos>9)
+            {
+                throw new ArgumentOutOfRangeException(nameof(NumDiscos));
+            }
+            PosteA = new Stack<byte>();
+            PosteB = new Stack<byte>();
+            PosteC = new Stack<byte>();
+
+            //Llena con valores de NumDiscos a 1
+            for (byte i = NumDiscos; i > 0 ; i--)
+            {
+                PosteA.Push(i);
+            }
+
+            EstaIniciado=true;
+        }
+
+        internal void Mover(char Origen, char Destino)
+        {
+            string postes = "ABC";
+            if (postes.IndexOf(Origen) == -1 || postes.IndexOf(Destino) == -1)
+            {
+                throw new ArgumentOutOfRangeException("El origen o el destino es incorrecto. (SOLO A, B o C)");
+            }
+            if (Origen == Destino)
+            {
+                return;
+            }
+
+            Stack<byte> posteOrigen = 
+                (Origen == 'A') ? PosteA : 
+                    (Origen == 'B') ? PosteB:PosteC ;
+            Stack<byte> posteDestino =
+                (Destino == 'A') ? PosteA :
+                    (Destino == 'B') ? PosteB : PosteC;
+
+            if (posteOrigen.Count==0)
+            {
+                throw new ArgumentException("El poste origen está vacío.");
+            }
+
+            byte valorOrigen = posteOrigen.Peek();
+            if (posteDestino.Count>0)
+            {
+                byte valorDestino = posteDestino.Peek();
+                if (valorOrigen>valorDestino)
+                {
+                    throw new ArgumentException("El disco origen es mayor que el destino");
+                }
+
+                posteDestino.Push(posteOrigen.Pop());
+            }
+            else
+            {
+                posteDestino.Push(posteOrigen.Pop());
+            }
+            
+        }
+    }
+}
